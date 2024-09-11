3243c5ab93d39fc5771fa39902f52cd9e2fa8187
Damian Boquete <damian.boquete@hotmail.com>
2024-09-11 16:29:05 +0200

    improved init file

--- init.sh
+++ init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-conkydir="$HOME/.config/conky/"
-
+conkydir="$HOME/.config/conky"
+baseurl="https://apod.nasa.gov/apod/"
+logfile="$conkydir/logs/$(date +'%Y%m%d%H%M%S').log"
 mkdir -p "$conkydir/logs"
-exec conky -d -c "$conkydir/conky.conf" &> "$conkydir/logs/$(date +'%Y%m%d%H%M%S').log"
+mkdir -p "$conkydir/img"
+
+echo "Launching conky, please wait..."
+exec &> "$logfile"
+
+imageurl=$(curl $baseurl | grep "<a href=\"image" | grep -oP '[0-9]+\/\w+.\w+')
+curl -o "$conkydir/img/image.jpg" "$baseurl/image/$imageurl"
+
+echo "Conky launched !" >&1
+
+conky -d -c "$conkydir/conky.conf"
