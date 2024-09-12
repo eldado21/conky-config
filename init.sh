b5ff9ed8c6d8e28517af82409a82d615e2890d6f
Damian Boquete <damian.boquete@hotmail.com>
2024-09-12 13:12:10 +0200

    Minor fix, added comment

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: find a way to make it work at startup (X starts after this script and conky cannot run properly) 
+
 conkydir="$HOME/.config/conky"
 baseurl="https://apod.nasa.gov/apod/"
 logfile="$conkydir/logs/$(date +'%Y%m%d%H%M%S').log"
@@ -12,6 +14,5 @@ exec &> "$logfile"
 imageurl=$(curl $baseurl | grep "<a href=\"image" | grep -oP '[0-9]+\/\w+.\w+')
 curl -o "$conkydir/img/image.jpg" "$baseurl/image/$imageurl"
 
-echo "Conky launched !" >&1
-
 conky -d -c "$conkydir/conky.conf"
+echo "Conky launched !"
