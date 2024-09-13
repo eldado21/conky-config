fb2def2ab37ab8b597dfc96cb067bbd4c5ea7cea
Damian Boquete <damian.boquete@hotmail.com>
2024-09-13 09:13:12 +0200

    Updated command to work on 2nd screen when run at startup

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: find a way to make it work at startup (X starts after this script and conky cannot run properly) 
-
 conkydir="$HOME/.config/conky"
 baseurl="https://apod.nasa.gov/apod/"
 logfile="$conkydir/logs/$(date +'%Y%m%d%H%M%S').log"
@@ -11,8 +9,8 @@ mkdir -p "$conkydir/img"
 echo "Launching conky, please wait..."
 exec &> "$logfile"
 
+# getting APOD
 imageurl=$(curl $baseurl | grep "<a href=\"image" | grep -oP '[0-9]+\/\w+.\w+')
 curl -o "$conkydir/img/image.jpg" "$baseurl/image/$imageurl"
 
-conky -d -c "$conkydir/conky.conf"
-echo "Conky launched !"
+conky --daemonize --xinerama-head=2 --config="$conkydir/conky.conf"
